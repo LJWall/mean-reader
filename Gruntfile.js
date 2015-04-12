beaf0b1e0b40027e17721f76333d1bf9f7877687
Liam Wall <liam.wall@gmail.com>
2015-04-12 19:30:34 +0100

    Futher tool setup.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    
+    var config = {};
+
+    // Load grunt packages and create black section of the config object.  
+    grunt.loadNpmTasks('grunt-contrib-copy'); 
+    config.copy = {};
+    grunt.loadNpmTasks('grunt-express-server'); 
+    config.express = {};
+    grunt.loadNpmTasks('grunt-contrib-watch'); 
+    config.watch = {};
+    grunt.loadNpmTasks('grunt-bower-concat');
+    config.bower_concat = {};
+
+    // Bower dependencies 
+    config.bower_concat.all = {dest: 'frontend_build/js/bower.js', cssDest: 'frontend_build/css/bower.css'};
+    config.watch.bower = {files: ['bower.json'], tasks: ['bower_concat:all']};
+    
+    // Own JavaScript
+    config.copy.js = {files: [{expand: true, filter: 'isFile', flatten: true, src: 'frontend-src/*.js', dest: 'frontend-build/'}] };
+    config.watch.js = {files: ['frontend-src/*.js'], tasks: ['copy:js']};
+
+    // HTML
+    config.copy.html = {files: [{expand: true, filter: 'isFile', flatten: true, src: 'frontend-src/*.html', dest: 'frontend-build/'}] };
+    config.watch.html = {files: ['frontend-src/*.html'], tasks: ['copy:html']};
+
+    // Server
+    config.express = { options: {script: 'backend/main.js'}, dev: {}},
+    config.watch.server = {files: ['backend/*.js'], tasks: ['express:dev:stop', 'express:dev'], options: {spawn: false}}
+
+    grunt.initConfig(config);
+    grunt.registerTask('build', ['bower_concat', 'copy']);
+    grunt.registerTask('default',['build']);
+    grunt.registerTask('run', ['build', 'express:dev', 'watch']);
+    
+};
