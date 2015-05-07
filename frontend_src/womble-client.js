0108d202e2c8007fb8de55a4ef3635aff54c6685
Liam Wall <liam.wall@gmail.com>
2015-05-07 18:05:29 +0100

    Refaactor front end slightly
    - Return values rather than promises from the service
    - Don't store any values in the controller.

--- frontend_src/womble-client.js
+++ frontend_src/womble-client.js
@@ -4,20 +4,27 @@
     var wombleApp = angular.module('wombleApp', []);
     
     wombleApp.factory('wombleService', ['$http', function ($http) {
-        var feed_data;
+        var meta_data = [];
+        var items = [];
+        
+        // Load all the data initally
+        $http.get('/reader').then(function (res) {
+            meta_data = res.data.meta;
+            items = res.data.items;
+        });
+        
         return {
-            postFeedUri: function (feedURI) {
-                console.log(feedURI);
-                $http.post('/womble', {feed_url: feedURI})
-                    .then(function (res) {console.log('Got results'); feed_data = res.data;},
-                          function (err) {console.log('Error: ', err)});
+            getFeedMetaList: function () {
+                return meta_data;
             },
-            getFeedData: function () {
-                if (!feed_data) {
-                    feed_data = $http.get('/womble')
-                                    .then(function (res) {console.log('Got results: ', res.data); return res.data;});
-                }
-                return feed_data;
+            getFeedItems: function () {
+                return items;
+            },
+            addNew: function (url) {
+                $http.post('/reader/add', {url: url}).then(function (res) {
+                    meta_data.push(res.data.meta);
+                    items = items.concat(res.data.items);
+                });
             }
         };
     }]);
@@ -25,23 +32,18 @@
     wombleApp.controller('wombleCtrl', ['wombleService', function (ws) {
         var self = this;
         
-        self.feedData = {meta: []};
-        self.updateDate = function () {
-            return ws.getFeedData().then(function (data) {
-                self.feedData = data;
-            });
+        self.feedData = {
+            meta: ws.getFeedMetaList,
+            items: ws.getFeedItems
         };
-        
-        self.updateDate().then(function () {
-            if (self.feedData.meta[0]) {
-                self.selected = self.feedData.meta[0].feedurl;
-            }
-            
-        });
         
         self.selectItem = function (post) {
             post.read = true;
             window.open(post.link, '_blank');
+        };
+        
+        self.addNew = function (url) {
+            ws.addNew(url)
         };
     }]);
     
