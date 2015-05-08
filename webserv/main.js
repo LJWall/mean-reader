f7b6fabc209c110e22f6de250fae39e38f8c3987
Liam Wall <liam.wall@gmail.com>
2015-05-08 16:11:37 +0100

    Correct order of express middleware

--- webserv/main.js
+++ webserv/main.js
@@ -4,10 +4,12 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 app = express();
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/reader', reader);
-app.use(morgan('dev'));
 app.use(express.static('frontend_build'));
+
 app.listen(5667);
 console.log('Listening on port 5667');
 
