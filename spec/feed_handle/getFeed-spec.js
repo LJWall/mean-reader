a89a6004537d8be8cec7fda7d1b0354b861b0100
Liam Wall <liam.wall@gmail.com>
2015-05-08 10:20:08 +0100

    Fix the spec for getFeed

--- spec/feed_handle/getFeed-spec.js
+++ spec/feed_handle/getFeed-spec.js
@@ -9,49 +9,63 @@ describe('getFeed', function () {
         items: [{title: 'post 1'}, {title: 'post 2'}]
     };
     
-    xit('should go to the source if not in the database (and store the result)', function (done) {
-        var db_contents = {meta: null, items: []};
-        spyOn(getFeedFromURL, 'get').and.returnValue(Promise.resolve(sample_data));
-        spyOn(mongoFeedStore, 'getMongoFeedMeta').and.callFake(function () {
-            return Promise.resolve(db_contents.meta)
-        });
-        spyOn(mongoFeedStore, 'getMongoFeedItems').and.callFake(function () {
-            return Promise.resolve(db_contents.items);
-        });
-        spyOn(mongoFeedStore, 'updateMongoFeedData').and.callFake(function (new_data) {
-            db_contents = new_data;
-            return Promise.resolve(1)
-        });
-        
-        getFeed.get('http://blog.rss')
-        .then(function (result) {
-            expect(mongoFeedStore.getMongoFeedMeta.calls.count()).toEqual(2);
-            expect(mongoFeedStore.getMongoFeedMeta.calls.allArgs()).toEqual([['http://blog.rss'], ['http://blog.rss']]);
-            expect(mongoFeedStore.getMongoFeedItems.calls.count()).toEqual(1);
-            expect(mongoFeedStore.getMongoFeedItems).toHaveBeenCalledWith('http://blog.rss');
-            expect(mongoFeedStore.updateMongoFeedData.calls.count()).toEqual(1);
-            expect(getFeedFromURL.get.calls.count()).toEqual(1);
-            expect(getFeedFromURL.get).toHaveBeenCalledWith('http://blog.rss');
-            expect(result).toEqual(sample_data);
-        })
-        .done(done);
+    it('should pass most calls straight through to mongoFeedStore', function () {
+        expect(getFeed.get).toBe(mongoFeedStore.getMongoFeedData);
+        expect(getFeed.getMeta).toBe(mongoFeedStore.getMongoFeedMeta);
+        expect(getFeed.getItems).toBe(mongoFeedStore.getMongoFeedItems);
+        expect(getFeed.getItemsByID).toBe(mongoFeedStore.getMongoFeedItemsByID);
+        expect(getFeed.setRead).toBe(mongoFeedStore.setRead);
     });
     
-    xit('should return database results if available (and and not go out the source)', function (done) {
-        sample_data.meta.last_update = new Date();
-        spyOn(getFeedFromURL, 'get');
-        spyOn(mongoFeedStore, 'getMongoFeedMeta').and.returnValue(Promise.resolve(sample_data.meta));
-        spyOn(mongoFeedStore, 'getMongoFeedItems').and.returnValue(Promise.resolve(sample_data.items));
+    
+    describe('.add', function () {
+        it('should go to the source if not in the database (and store the result)', function (done) {
+            var db_contents = {meta: null, items: []};
+            spyOn(getFeedFromURL, 'get').and.returnValue(Promise.resolve(sample_data));
+            spyOn(mongoFeedStore, 'getMongoFeedMeta').and.callFake(function () {
+                return Promise.resolve(db_contents.meta)
+            });
+            spyOn(mongoFeedStore, 'getMongoFeedItems').and.callFake(function () {
+                return Promise.resolve(db_contents.items);
+            });
+            spyOn(mongoFeedStore, 'getMongoFeedData').and.callFake(function () {
+                return Promise.resolve(db_contents);
+            });
+            spyOn(mongoFeedStore, 'updateMongoFeedData').and.callFake(function (new_data) {
+                db_contents = new_data;
+                return Promise.resolve(1)
+            });
+            
+            getFeed.add('http://blog.rss')
+            .then(function (result) {
+                expect(mongoFeedStore.getMongoFeedMeta.calls.count()).toEqual(1);
+                expect(mongoFeedStore.getMongoFeedMeta).toHaveBeenCalledWith('http://blog.rss');
+                expect(getFeedFromURL.get.calls.count()).toEqual(1);
+                expect(getFeedFromURL.get).toHaveBeenCalledWith('http://blog.rss');
+                expect(mongoFeedStore.updateMongoFeedData.calls.count()).toEqual(1);
+                expect(mongoFeedStore.getMongoFeedData.calls.count()).toEqual(1);
+                expect(mongoFeedStore.getMongoFeedData).toHaveBeenCalledWith('http://blog.rss');
+                expect(result).toEqual(sample_data);
+            })
+            .done(done);
+        });
         
-        getFeed.get('http://blog.rss')
-        .then(function (result) {
-            expect(getFeedFromURL.get.calls.count()).toEqual(0);
-            expect(mongoFeedStore.getMongoFeedMeta.calls.count()).toEqual(1);
-            expect(mongoFeedStore.getMongoFeedMeta).toHaveBeenCalledWith('http://blog.rss');
-            expect(mongoFeedStore.getMongoFeedItems.calls.count()).toEqual(1);
-            expect(mongoFeedStore.getMongoFeedItems).toHaveBeenCalledWith('http://blog.rss');
-            expect(result).toEqual(sample_data);
-        })
-        .done(done);
+        it('should return database results if available (and and not go out the source)', function (done) {
+            sample_data.meta.last_update = new Date();
+            spyOn(getFeedFromURL, 'get');
+            spyOn(mongoFeedStore, 'getMongoFeedMeta').and.returnValue(Promise.resolve(sample_data.meta));
+            spyOn(mongoFeedStore, 'getMongoFeedItems').and.returnValue(Promise.resolve(sample_data.items));
+            
+            getFeed.add('http://blog.rss')
+            .then(function (result) {
+                expect(getFeedFromURL.get.calls.count()).toEqual(0);
+                expect(mongoFeedStore.getMongoFeedMeta.calls.count()).toEqual(1);
+                expect(mongoFeedStore.getMongoFeedMeta).toHaveBeenCalledWith('http://blog.rss');
+                expect(mongoFeedStore.getMongoFeedItems.calls.count()).toEqual(1);
+                expect(mongoFeedStore.getMongoFeedItems).toHaveBeenCalledWith('http://blog.rss');
+                expect(result).toEqual(sample_data);
+            })
+            .done(done);
+        });
     });
 });
