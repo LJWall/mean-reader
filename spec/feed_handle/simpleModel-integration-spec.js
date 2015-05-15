5fa182cabab89f2773610e550cbd97a2da5dba0b
Liam Wall <liam.wall@gmail.com>
2015-05-15 13:15:42 +0100

    Finish the integration spec for simpleModel

--- spec/feed_handle/simpleModel-integration-spec.js
+++ spec/feed_handle/simpleModel-integration-spec.js
@@ -38,7 +38,7 @@ describe('simpleModel [integration]', function () {
         .then(function (result) {
             expect(result).toEqual(jasmine.objectContaining({foo: 2, bar: 'b'}));
             result.more = {hello: 'world'};
-            return result.save()
+            return result.save();
         })
         .then(function () {
             return collection.find({}).toArrayAsync();
@@ -52,8 +52,24 @@ describe('simpleModel [integration]', function () {
         .done(done);
     });
     it('should get results with .findMany() and save a change with .save()', function (done) {
-        pending();
-        done();
+        model.findMany({}, {foo: -1}, 2)
+        .then(function (result) {
+            expect(result.length).toEqual(2);
+            expect(result[0]).toEqual(jasmine.objectContaining({foo: 3, bar: 'c'}));
+            expect(result[1]).toEqual(jasmine.objectContaining({foo: 2, bar: 'b'}));
+            result[0].more = {tea: 'time'};
+            return result[0].save();
+        })
+        .then(function () {
+            return collection.find({}).toArrayAsync();
+        })
+        .then(function (result) {
+            expect(result.length).toEqual(3);
+            expect(result).toContain({_id: 100, foo: 1, bar: 'a'});
+            expect(result).toContain({_id: 101, foo: 2, bar: 'b'});
+            expect(result).toContain({_id: 102, foo: 3, bar: 'c', more: {tea: 'time'}});
+        })
+        .done(done);
     });
     
 });
