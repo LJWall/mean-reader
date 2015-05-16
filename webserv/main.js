647ef2ac6e93aae9fec6adfaae82a54f3940f42a
Liam Wall <liam.wall@gmail.com>
2015-05-16 21:32:38 +0100

    Tweak main to account for api_routes module chnage

--- webserv/main.js
+++ webserv/main.js
@@ -1,13 +1,14 @@
 var express = require('express'),
-    reader = require('./mean-reader'),
+    api_routes = require('./api_routes'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use('/reader', reader);
+app.use('/reader', api_routes('/reader'));
 app.use(express.static('frontend_build'));
+
 
 app.listen(5667);
 console.log('Listening on port 5667');
