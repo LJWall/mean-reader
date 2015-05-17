bd60bdbe2f64d80300f09ca0675e1d1979a44961
Liam Wall <liam.wall@gmail.com>
2015-05-17 10:02:57 +0100

    Update the feed update script in line with other chnages

--- webserv/updateFeeds.js
+++ webserv/updateFeeds.js
@@ -1,11 +1,13 @@
 var getFeedFromUrl = require('./feed_handle/utils/getFeedFromURL.js'),
+    feedModel = require('./feed_handle/getFeed.js')(),
     mongoFeedStore = require('./feed_handle/utils/mongoFeedStore.js'),
     mongoConn = require('./mongoConnect.js');
+    db = mongoConn.connection();
     
-mongoFeedStore.getMongoFeedMeta()
+feedModel.feeds.findMany({})
 .each(function (meta, index, value)  {
     console.log('Updating', meta.title)
-    return getFeedFromUrl.get(meta.feedurl).then(mongoFeedStore.updateMongoFeedData);
+    return getFeedFromUrl.get(meta.feedurl).then(mongoFeedStore.updateMongoFeedData.bind(null, db));
 })
 .then(function () {
     console.log('Done')
