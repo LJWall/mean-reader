ada58064e3c800f165bea269d501ae42a0f6f79b
Liam Wall <liam.wall@gmail.com>
2015-05-29 21:15:34 +0100

    Exceptions handling unthe updateFeeds script

--- webserv/updateFeeds.js
+++ webserv/updateFeeds.js
@@ -1,16 +1,23 @@
 var getFeedFromUrl = require('./feed_handle/utils/getFeedFromURL.js'),
     feedModel = require('./feed_handle/getFeed.js')(),
     mongoFeedStore = require('./feed_handle/utils/mongoFeedStore.js'),
-    mongoConn = require('./mongoConnect.js');
+    mongoConn = require('./mongoConnect.js'),
     db = mongoConn.connection();
     
 feedModel.feeds.findMany({})
 .each(function (meta, index, value)  {
-    console.log('Updating', meta.title)
-    return getFeedFromUrl.get(meta.feedurl).then(mongoFeedStore.updateMongoFeedData.bind(null, db));
+    return getFeedFromUrl.get(meta.feedurl)
+        .then(mongoFeedStore.updateMongoFeedData.bind(null, db))
+        .then(function () {
+            console.log('Updated', meta.title);
+        })
+        .catch(function (err) {
+            console.log('Error with', meta.title, 'Error', err);
+        });
 })
 .then(function () {
     console.log('Done')
     mongoConn.disconnect();
 })
 .done();
+
