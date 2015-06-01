817ee141fbebf6b4f274fad1bd6fca2055962864
Liam Wall <liam.wall@gmail.com>
2015-06-01 20:08:26 +0100

    Use angular directive for login/out functionality

--- frontend_src/user.js
+++ frontend_src/user.js
@@ -3,14 +3,55 @@
 var ME_URL = '/reader/auth/me';
 
 angular.module('reader.user', [])
-.run(['$http', function ($http) {
-    var me = $http.get(ME_URL);
-    me.then(function (data) {
-        console.log(data);
-    } );
-    //return {
-    //    me: function () { return me; }
-    //};
+.service('currentUserService', ['$http', function ($http) {
+    var userData;
+
+    activate();
+
+    return {
+        data: function () { return userData; },
+        isAuthenticated: function () {
+            return (userData ? true : false);
+        },
+        logout: logout
+    };
+
+    function activate() {
+        updateUser();
+    }
+
+    function updateUser() {
+        $http.get(ME_URL)
+        .success(function (res) {
+            userData = res;
+        })
+        .error(function () {
+            userData = null;
+        });
+    }
+    function logout () {
+        $http.get('/reader/auth/logout')
+        .then(function () {
+            updateUser();
+        });
+    }
+}])
+.controller('CurrentUserCtrl', ['currentUserService', function (userService) {
+    this.data = function () {return userService.data(); };
+    this.isAuthenticated = function () {return userService.isAuthenticated(); };
+    this.logout = function () {return userService.logout(); };
+}])
+.directive('readerLogin', [function (currentUser) {
+    return {
+        template:   '<div ng-controller="CurrentUserCtrl as c">' +
+            '<span ng-show="c.isAuthenticated()">{{c.data().displayName}} ' +
+            '<a href ng-click="c.logout()">Log out</a>' +
+            '</span>' +
+            '<span ng-show="!c.isAuthenticated()"><a href="/reader/auth/google">Log in with Google</a></span>' +
+            '</div>',
+        restrict: 'AE',
+        scope: {}
+    };
 }]);
 
 })();
