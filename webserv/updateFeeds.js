1d19dcf91cb8ab2a0f6b6c8f9db7a845170aaba8
Liam Wall <liam.wall@gmail.com>
2015-06-06 18:59:11 +0100

    Update updateFeeds to account for users

--- webserv/updateFeeds.js
+++ webserv/updateFeeds.js
@@ -1,19 +1,27 @@
 var getFeedFromUrl = require('./feed_handle/utils/getFeedFromURL.js'),
-    feedModel = require('./feed_handle/getFeed.js')(),
     mongoFeedStore = require('./feed_handle/utils/mongoFeedStore.js'),
     mongoConn = require('./mongoConnect.js'),
-    db = mongoConn.connection();
-    
-feedModel.feeds.findMany({})
-.each(function (meta, index, value)  {
-    return getFeedFromUrl.get(meta.feedurl)
-        .then(mongoFeedStore.updateMongoFeedData.bind(null, db))
-        .then(function () {
-            console.log('Updated', meta.title);
-        })
-        .catch(function (err) {
-            console.log('Error with', meta.title, 'Error', err);
+    db = mongoConn.connection(),
+    Promise = require('bluebird');
+
+db.call('collection', 'feeds')
+.call('aggregateAsync', [
+    {$project: {feedurl: true, user_id:true}},
+    {$group: {_id: {feedurl: '$feedurl'}, user_ids: {$addToSet: '$user_id'} }}
+])
+.each(function (feed, index, value)  {
+    return getFeedFromUrl.get(feed._id.feedurl)
+    .then(function (feed_data) {
+        return Promise.each(feed.user_ids, function (uid) {
+            return mongoFeedStore.updateMongoFeedData(db, feed_data, uid);
         });
+    })
+    .then(function () {
+        console.log('Updated', feed._id.feedurl);
+    })
+    .catch(function (err) {
+        console.log('Error with', feed._id.feedurl, 'Error', err);
+    });
 })
 .then(function () {
     console.log('Done');
