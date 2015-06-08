128855ed2b8533afb2b00404d1c6efa5f9f18a8a
Liam Wall <liam.wall@gmail.com>
2015-06-08 20:22:22 +0100

    encapsulate all function in new Promise.
    Ensure any syncronous errors result in Promise.reject
    rather than actually throwing.

--- webserv/feed_handle/utils/findAlternates.js
+++ webserv/feed_handle/utils/findAlternates.js
@@ -3,27 +3,27 @@ var sax = require('sax'),
     Promise = require('bluebird');
 
 module.exports = function (req) {
-    var feeds={},
-        promise,
-        saxParser = sax.createStream(false, {lowercase: true});
-
-    saxParser.on('opentag', function (node) {
-        if (node.name==='link' && node.attributes.rel==='alternate') {
-            if (node.attributes.type === 'application/rss+xml' || node.attributes.type === 'application/atom+xml') {
-                feeds[node.attributes.type] = node.attributes.href;
-            }
-        }
-    });
-
     promise = new Promise(function (resolve, reject) {
+        var feeds={},
+            promise,
+            saxParser = sax.createStream(false, {lowercase: true});
+
+        saxParser.on('opentag', function (node) {
+            if (node.name==='link' && node.attributes.rel==='alternate') {
+                if (node.attributes.type === 'application/rss+xml' || node.attributes.type === 'application/atom+xml') {
+                    feeds[node.attributes.type] = node.attributes.href;
+                }
+            }
+        });
+
         saxParser.on('error', function (e) {
             reject(e);
         });
         saxParser.on('end', function () {
             resolve(feeds);
         });
+        req.pipe(saxParser);
     });
 
-    req.pipe(saxParser);
     return promise;
 };
