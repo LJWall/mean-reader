864a991c0424fde915b275a9fade6561ef41292f
Liam Wall <liam.wall@gmail.com>
2015-06-08 23:05:12 +0100

    Improve static file server used in testing..

--- spec/feed_handle/test_atomserv/feed_server.js
+++ spec/feed_handle/test_atomserv/feed_server.js
@@ -1,32 +1,9 @@
 var app;
 
 module.exports.startServer = function () {
-    var express = require('express'),
-        url = require('url'),
-        fs = require('fs');
+    var express = require('express');
     
     app = express();
-    
-    app.get('/*', function (req, res) {
-        var path = url.parse(req.url).pathname;
-        var feed_stream, body;
-        
-        feed_stream = fs.createReadStream(__dirname + '/' + path);
-        
-        feed_stream.on('data', function (chunck) {
-            body += chunck.toString();
-        })
-        .on('error', function () {
-            //res.writeHead(404)
-            res.end();
-            console.log('404 GET: ', path);
-        })
-        .on('end', function () {
-            res.writeHead(200, {'content-type': 'application/atom+xml'});
-            res.end(body);
-            //console.log('200 GET: ', path);
-        });
-    });
-    
+    app.use(express.static(__dirname));
     app.listen(1337);
 };
