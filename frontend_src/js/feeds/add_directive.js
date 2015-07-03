2073dd41f67a74aec83aaacffefb073bca5705a2
Liam Wall <liam.wall@gmail.com>
2015-07-03 23:58:28 +0100

    Show multiple log message

--- frontend_src/js/feeds/add_directive.js
+++ frontend_src/js/feeds/add_directive.js
@@ -5,13 +5,24 @@ angular.module('reader.feeds')
         restrict: 'AE',
         scope: {},
         link: function ($scope) {
+            $scope.messages = [];
             $scope.addNew = function (url) {
-                $scope.alertmsg = null;
+                var newMsg = {
+                    'text': 'Adding ' + url,
+                    'class': 'alert alert-info'
+                };
+                $scope.messages.push(newMsg);
+                console.log($scope.messages);
                 feedService.addNew(url)
-                .then(null, function () {
-                    $scope.alertmsg = 'Hmmm.  Theres seems to be a problem with ' + url +
+                .then(function () {
+                    newMsg.text = 'Added ' + url;
+                    newMsg.class = 'alert alert-success';
+                })
+                .catch(function () {
+                    newMsg.text = 'Hmmm.  Theres seems to be a problem with ' + url +
                         '. Maybe try again a bit later. ' +
                         'If it still doesn\'t work drop an email to ljw.dev@gmail.com.';
+                    newMsg.class = 'alert alert-danger';
                 });
             };
          }
