47d48abeb26128be0ca013a0be5ada0ba510d767
Liam Wall <liam.wall@gmail.com>
2015-07-03 07:58:23 +0100

    Continue refacctor db handling

--- spec/feed_handle/getFeed-spec.js
+++ spec/feed_handle/getFeed-spec.js
@@ -1,58 +1,24 @@
 var rewire = require('rewire'),
     mongoFeedStore = require('../../webserv/feed_handle/utils/mongoFeedStore.js'),
     getFeedFromURL = require('../../webserv/feed_handle/utils/getFeedFromURL.js'),
-    simpleModel = require('../../webserv/feed_handle/utils/simpleModel.js'),
     getFeedMaker = rewire('../../webserv/feed_handle/getFeed.js'),
     getFeed,
     Promise = require('bluebird'),
-    mongoConn = require('../../webserv/mongoConnect.js');
+    db = require('../../webserv/mongoConnect.js');
 
 describe('getFeed', function () {
-    var fakeDb, findOneSpy;
+    var fakeDb;
     beforeAll(function () {
-        fakeDb = {
-            collection: function (name) {return {'name': name}; },
-            call: function (method, param) {return fakeDb[method](param); }
-        };
-        getFeedMaker.__set__('mongoConnect', {connection: fakeDb});
-
-        findOneSpy = jasmine.createSpy('findOneSpy').and.callFake(function (query) {
-                        if (query.feedurl === 'http://cannonical' || (mongoFeedStore.updateMongoFeedData.calls.count() >0 && query.feedurl === 'eggs' )) {
-                            query._id = 'spam';
-                            return Promise.resolve(query);
-                        }
-                        return Promise.resolve(null);
-        });
-
-        spyOn(simpleModel, 'make').and.callFake(function (collection) {
-            if (collection.name === 'feeds') {
-                return {
-                    findOne: findOneSpy,
-                    collection: collection
-                };
-            } else {
-                return {collection: collection};
-            }
-        });
-        
         getFeed = getFeedMaker();
     });
-    
-    it('should use simpleModel to make feeds object', function () {
-        expect(simpleModel.make).toHaveBeenCalledWith({name: 'feeds'});
-        expect(getFeed.feeds.collection).toEqual({name: 'feeds'});
-    });
-    
-    it('should use simpleModel to make posts object', function () {
-        expect(simpleModel.make).toHaveBeenCalledWith({name: 'posts'});
-        expect(getFeed.posts.collection).toEqual({name: 'posts'});
-    });
-    
+
     describe('.add', function () {
         beforeAll(function () {
             this.fakeFeedData = {meta: {feedurl: 'http://cannonical'}};
             this.getFeedFromURLSpy = jasmine.createSpy('getFeedFromURL').and.returnValue(Promise.resolve(this.fakeFeedData));
             getFeedMaker.__set__('getFeedFromURL', this.getFeedFromURLSpy);
+            spyOn(db.feeds, 'findOneAsync');
+            spyOn(mongoFeedStore, 'updateMongoFeedData');
         });
         beforeEach(function () {
             this.getFeedFromURLSpy.calls.reset();
@@ -60,13 +26,13 @@ describe('getFeed', function () {
 
         it('should go to the source (and store the result)', function (done) {
             var self = this;
-            spyOn(mongoFeedStore, 'updateMongoFeedData');
             getFeed.add('eggs', 'FOOBAR')
             .then(function (result) {
                 expect(self.getFeedFromURLSpy.calls.allArgs()).toEqual([['eggs', true, true]]);
                 expect(mongoFeedStore.updateMongoFeedData.calls.count()).toEqual(1);
-                expect(mongoFeedStore.updateMongoFeedData).toHaveBeenCalledWith(fakeDb, self.fakeFeedData, 'FOOBAR');
-                expect(result).toEqual({feedurl: 'http://cannonical', _id: 'spam'});
+                expect(mongoFeedStore.updateMongoFeedData).toHaveBeenCalledWith(db.connection, self.fakeFeedData, 'FOOBAR');
+                expect(db.feeds.findOneAsync.calls.count()).toEqual(1);
+                expect(db.feeds.findOneAsync.calls.argsFor(0)).toEqual([{'feedurl': 'http://cannonical', user_id: 'FOOBAR'}]);
             })
             .done(done);
         });
--- webserv/feed_handle/getFeed.js
+++ webserv/feed_handle/getFeed.js
@@ -1,24 +1,20 @@
 var getFeedFromURL = require('./utils/getFeedFromURL.js'),
     mongoFeedStore = require('./utils/mongoFeedStore.js'),
-    mongoConnect = require('../mongoConnect.js'),
+    db = require('../mongoConnect.js'),
     simpleModel = require('./utils/simpleModel.js'),
     Promise = require('bluebird');
 
 module.exports = function () {
-    var obj = {},
-        db_promise = mongoConnect.connection;
-        
-    obj.feeds = simpleModel.make(db_promise.call('collection', 'feeds'));
-    obj.posts = simpleModel.make(db_promise.call('collection', 'posts'));
+    var obj = {};
     obj.add = function (url, user_id) {
         var newUrl;
         return getFeedFromURL(url, true, true)
         .then(function (data) {
             newUrl = data.meta.feedurl;
-            return mongoFeedStore.updateMongoFeedData(db_promise, data, user_id);
+            return mongoFeedStore.updateMongoFeedData(db.connection, data, user_id);
         })
         .then(function () {
-            return obj.feeds.findOne({'feedurl': newUrl});
+            return db.feeds.findOneAsync({'feedurl': newUrl, user_id: user_id});
         });
     };
     
