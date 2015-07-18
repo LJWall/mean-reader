9b0b6ce63521a168c8eea24db5bd3fe59e0aced4
Liam Wall <liam.wall@gmail.com>
2015-07-18 21:19:19 +0100

    Keep images in their box
    and, add a (link) headline to item content

--- frontend_src/js/feeds/item_directive.js
+++ frontend_src/js/feeds/item_directive.js
@@ -11,17 +11,18 @@ angular.module('reader.feeds')
             $scope.show = function () {
                 $scope.show_content = !$scope.show_content;
                 if ($scope.show_content) {
+                    var titleLink = '<h4><a href="' + $scope.item.link + '" target="_blank">' + $scope.item.title + '</a></h4>';
                     $scope.item.markAsRead(true);
                     if (!$scope.item.content_apiurl) {
-                        $scope.content = $sce.trustAsHtml('<h3><a href="' + $scope.item.link + '" target="_blank">' + $scope.item.title + '</a></h3>');
+                        $scope.content = $sce.trustAsHtml(titleLink);
                     } else {
                         $scope.content = $sce.trustAsHtml('<h3><i class="fa fa-circle-o-notch fa-spin"></i> loading </h3>');
                         $scope.item.getContent()
                         .then(function (content) {
-                            $scope.content = $sce.trustAsHtml(content);
+                            $scope.content = $sce.trustAsHtml(titleLink + content);
                         })
                         .then(null, function (err) {
-                            $scope.content = $sce.trustAsHtml('<h3>Problem getting content</h3>');
+                            $scope.content = $sce.trustAsHtml(titleLink + '<p>Problem getting content.</p>');
                         });
                     }
                 }
