6b7291c80a60f78769f8567b19f0eb11874fb70d
Liam Wall <liam.wall@gmail.com>
2015-08-16 16:13:33 +0100

    Redirect to login if session is lost

--- frontend_src/js/user/user_service.js
+++ frontend_src/js/user/user_service.js
@@ -1,13 +1,12 @@
 (function () {
 
-var ME_URL = '/reader/auth/me';
+var ME_URL = '/reader/auth/me',
+    userData,
+    signOutCallbacks = [];
 
 angular.module('reader.user')
 .service('currentUserService', ['$http', function ($http) {
-    var userData, signOutCallbacks = [];
-
     updateUser();
-
     return {
         data: function () { return userData; },
         isAuthenticated: function () {
@@ -38,6 +37,26 @@ angular.module('reader.user')
     function onSignOut (callback) {
         signOutCallbacks.push(callback);
     }
+}])
+
+.factory('currentUserService_resTest', ['$q', function ($q) {
+    return {
+      responseError: function (res) {
+          if (res.status === 401) {
+              if (userData) {
+                signOutCallbacks.forEach(function (callback) {
+                    callback();
+                });
+              }
+              userData = null;
+          }
+          return $q.reject(res);
+      }
+    };
+}])
+
+.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('currentUserService_resTest');
 }]);
 
 })();
