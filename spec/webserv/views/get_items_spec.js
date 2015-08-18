665010fa4e749a13583735dc76931e75548293a9
Liam Wall <liam.wall@gmail.com>
2015-08-18 22:10:34 +0100

    Backend get_items complete (for now)

--- spec/webserv/views/get_items_spec.js
+++ spec/webserv/views/get_items_spec.js
@@ -4,7 +4,7 @@ var get_items = require('../../../webserv/views/get_items'),
     url_for = require('../../../webserv/url_for'),
     clean = require('../../../webserv/views/cleaning_util');
 
-describe('get_item', function () {
+describe('get_items', function () {
     beforeAll(util.prepTestData);
     beforeAll(util.insertTestData);
     beforeAll(util.makeResSpy);
@@ -35,6 +35,41 @@ describe('get_item', function () {
             done();
         });
         get_items({user: {_id: 'IMPOSTER'}, query: {}, params: {}}, this.spyRes);
+    });
+
+    it('should respect limit via N query parameter', function (done) {
+        var self = this;
+        this.spyRes.events.once('responseComplete', function () {
+            expect(self.spyRes.json.calls.count()).toEqual(1);
+            var data = self.spyRes.json.calls.argsFor(0)[0];
+            expect(data.items.length).toEqual(1);
+            done();
+        });
+        get_items({user: {_id: 'IMPOSTER'}, query: {N: 1}, params: {}}, this.spyRes);
+    });
+
+    it('should respect starred=true query parameter', function (done) {
+        var self = this;
+        this.spyRes.events.once('responseComplete', function () {
+            expect(self.spyRes.json.calls.count()).toEqual(1);
+            var data = self.spyRes.json.calls.argsFor(0)[0];
+            expect(data.items.length).toEqual(1);
+            expect(data.items[0]).toEqual(clean.cleanItem(test_data.item[2]));
+            done();
+        });
+        get_items({user: {_id: 'IMPOSTER'}, query: {starred: true}, params: {}}, this.spyRes);
+    });
+
+    it('should respect starred=false query parameter', function (done) {
+        var self = this;
+        this.spyRes.events.once('responseComplete', function () {
+            expect(self.spyRes.json.calls.count()).toEqual(1);
+            var data = self.spyRes.json.calls.argsFor(0)[0];
+            expect(data.items.length).toEqual(1);
+            expect(data.items[0]).toEqual(clean.cleanItem(test_data.item[1]));
+            done();
+        });
+        get_items({user: {_id: 'IMPOSTER'}, query: {starred: false}, params: {}}, this.spyRes);
     });
 });
 
