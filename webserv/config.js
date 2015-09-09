926cdd847387e12933d340f296a265e3546d35d9
Liam Wall <liam.wall@gmail.com>
2015-09-09 20:02:54 +0100

    Convert updateFeeds to simple one-time script
    - Will use cron to schdule, rather that keeping it running as
      a service with forever

--- webserv/config.js
+++ webserv/config.js
@@ -1,16 +1,10 @@
 
 if (process.env.MEAN_ENV === 'test') {
     exports.mongo_uri = 'mongodb://127.0.0.1:27017/testwomble';
-    exports.update_initwait = 1;
-    exports.update_loopwait = false;
 } else if (process.env.MEAN_ENV === 'development') {
     exports.mongo_uri = 'mongodb://127.0.0.1:27017/meanfeed';
-    exports.update_initwait = 1;
-    exports.update_loopwait = false;
 } else if (process.env.MEAN_ENV === 'production') {
     exports.mongo_uri = 'mongodb://127.0.0.1:27017/meanfeed';
-    exports.update_initwait = 30*60*100;
-    exports.update_loopwait = 2*60*60*1000;
 } else {
     console.log('Error Environment variable MEAN_ENV not set.');
     console.log('Sould be set to one of \'test\', \'development\', or \'production\'. ');
--- webserv/updateFeeds.js
+++ webserv/updateFeeds.js
@@ -5,70 +5,49 @@ var getFeedFromUrl = require('./feed_handle/utils/getFeedFromURL.js'),
     Promise = require('bluebird'),
     request = Promise.promisifyAll(require('request'));
 
-
-
-setTimeout(updateLoop, config.update_initwait);
-
-function updateLoop () {
-    updateFeeds()
-    .then(function () {
-        if (config.update_loopwait) {
-            setTimeout(updateLoop, config.update_loopwait);
-        } else {
-            db.disconnect();
+db.feeds
+.call('aggregateAsync', [
+    {$project: {feedurl: true, user_id:true, last_update: true}},
+    {$group: {_id: {feedurl: '$feedurl'}, user_ids: {$addToSet: '$user_id'}, last_update: {$min: '$last_update'} }}
+])
+.each(function (feed, index, value)  {
+    return request.headAsync(feed._id.feedurl)
+    .then(function (results) {
+        var last_mod;
+        last_mod = new Date(results[0].headers['last-modified']);
+        if (!isNaN(last_mod.getTime()) && feed.last_update.getTime() > last_mod.getTime()) {
+            // Not sure it's idiomatic to use errors for control flow
+            // in JS, but hey ho...
+            throw new Error('No recent modifications');
         }
     })
-    .done();
-}
+    .then(getFeedFromUrl.bind(null, feed._id.feedurl, false))
+    .then(function (feed_data) {
+        /* Write over the returned feed URL, incase it's been replaced with a differnt
+           cannonical URL */
+        feed_data.meta.feedurl = feed._id.feedurl;
 
-function updateFeeds () {
-    return db.feeds
-    .call('aggregateAsync', [
-        {$project: {feedurl: true, user_id:true, last_update: true}},
-        {$group: {_id: {feedurl: '$feedurl'}, user_ids: {$addToSet: '$user_id'}, last_update: {$min: '$last_update'} }}
-    ])
-    .each(function (feed, index, value)  {
-        return request.headAsync(feed._id.feedurl)
-        .then(function (results) {
-            var last_mod;
-            try {
-                last_mod = new Date(results[0].headers['last-modified']);
-            } catch (e) {
-                return;
-            }
-            if (feed.last_update.getTime() > last_mod.getTime()) {
-                // Not sure it's idiomatic to use errors for control flow
-                // in JS, but hey ho...
-                throw new Error('No recent modifications');
-            }
-        })
-        .then(getFeedFromUrl.bind(null, feed._id.feedurl, false))
-        .then(function (feed_data) {
-            /* Write over the returned feed URL, incase it's been replaced with a differnt
-               cannonical URL */
-            feed_data.meta.feedurl = feed._id.feedurl;
-
-            return mongoFeedStore.saveFeedItemContent(feed_data)
-            .then(function () {
-                return  feed_data;
-            });
-        })
-        .then(function (feed_data) {
-            return Promise.each(feed.user_ids, function (uid) {
-                return mongoFeedStore.updateMongoFeedData(feed_data, uid);
-            });
-        })
-        .then(log.bind(null, 'Updated ' + feed._id.feedurl))
-        .catch(function (err) {
-            log('Not updating ' + feed._id.feedurl + '. Reason: ' + err.message);
+        return mongoFeedStore.saveFeedItemContent(feed_data)
+        .then(function () {
+            return  feed_data;
         });
     })
-    .then(log.bind(null, 'Done for now'))
+    .then(function (feed_data) {
+        return Promise.each(feed.user_ids, function (uid) {
+            return mongoFeedStore.updateMongoFeedData(feed_data, uid);
+        });
+    })
+    .then(log.bind(null, 'Updated ' + feed._id.feedurl))
     .catch(function (err) {
-        log('Aborting current updates. Error' + err.message);
-        console.log(err.stack);
+        log('Not updating ' + feed._id.feedurl + '. Reason: ' + err.message);
     });
-}
+})
+.then(log.bind(null, 'Done for now'))
+.catch(function (err) {
+    log('Aborting current updates. Error' + err.message);
+    console.log(err.stack);
+})
+.then(db.disconnect);
 
 function log (msg) {
     var dt = new Date();
